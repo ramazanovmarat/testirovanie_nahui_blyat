ad1ba4f4a12acaede6f30c8d71f8e3c4dfef85e5
ramazanovmarat <magomedovramazan0834@gmail.com>
2026-01-15 19:34:43 +0300

    Temp: сохраняю работу перед обновлением

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello nahui blyat'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
